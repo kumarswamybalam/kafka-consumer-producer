85c8ab0dc3da626f628e9f7ea47b77aa0b8f3241
Kumar Swamy Balam <kumarswamybalam@Kumars-MacBook-Air.local>
2023-05-22 15:18:22 -0500

    initial commit

---
+++ src/test/java/com/balam/kafka/services/kafkaconsumerproducer/KafkaConsumerProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balam.kafka.services.kafkaconsumerproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaConsumerProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
