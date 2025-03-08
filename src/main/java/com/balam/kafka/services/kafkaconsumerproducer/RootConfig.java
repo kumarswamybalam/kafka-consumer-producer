12843665a3798af6a6084d3bf897962449cc172f
kumarswamybalam <kumarswamybalam@gmail.com>
2025-03-08 14:14:39 -0600

    added docker-compose

--- src/main/java/com/balam/kafka/services/kafkaconsumerproducer/RootConfig.java
+++ src/main/java/com/balam/kafka/services/kafkaconsumerproducer/RootConfig.java
@@ -1,8 +1,14 @@
 package com.balam.kafka.services.kafkaconsumerproducer;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+import java.time.Clock;
 
 @Import({})
 @Configuration
@@ -10,5 +16,18 @@ public class RootConfig {
 
     static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
+    }
+
+    @Bean
+    Clock clock() {
+        return Clock.systemDefaultZone();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        JavaTimeModule module = new JavaTimeModule();
+        return new ObjectMapper()
+                .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+                .registerModule(module);
     }
 }
